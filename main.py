9015739bb32deb02d5fcf2b3ed92a7b2ead311e1
Dulara Rathnayake <87459380+Dulara2001@users.noreply.github.com>
2023-06-24 19:43:15 +0530

    add max lines function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+MAX_LINES = 3
+
 def deposit():
-    while True:
+    while True:     #continue to do this until break
         amount = input("What would you like to deposit? $")
         if amount.isdigit():
             amount = int(amount)
@@ -7,4 +9,34 @@ def deposit():
                 break
             else:
                 print("Amount must be greater than 0.")
-        
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+
+
+def get_number_of_lines():
+    while True:
+        lines = input(
+            "Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance,lines)
+
+main()
