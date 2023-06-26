43581706d4433f96ef7cc3b6f1348b53bda6fab4
Dulara Rathnayake <87459380+Dulara2001@users.noreply.github.com>
2023-06-26 13:20:31 +0530

    add print slot machine function

--- main.py
+++ main.py
@@ -36,6 +36,15 @@ def get_slot_machine_spin(rows, cols , symbols):
     return columns   
 
 
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i,column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+            
+        print()
    
 
 
@@ -104,5 +113,7 @@ def main():
     
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
 
+    slots = get_slot_machine_spin(ROWS , COLS , symbol_count)
+    print_slot_machine(slots)
 
 main()
